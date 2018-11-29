e697698ec08cc5c44068d5fb548ce21e21d88590
unknown <bafford1234@gmail.com>
2018-11-29 10:47:28 -0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const pButton = document.getElementById("pButton");
+const gifButton = document.getElementById("gifButton");
+
+pButton.addEventListener("click", () => {
+  document.querySelector("#Pakter").classList.add("show");
+})
+
+gifButton.addEventListener("click", () => {
+})
