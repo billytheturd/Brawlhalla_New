05d4d82a3f4c6016c63107458deb46590830b6f4
unknown <bafford1234@gmail.com>
2018-12-13 10:49:30 -0500

    heres my push

--- javascript.js
+++ javascript.js
@@ -1,9 +1,42 @@
-const pButton = document.getElementById("pButton");
-const gifButton = document.getElementById("gifButton");
+const brawl1 = $("#Brawl1");
+const button = $("#gifButton");
+const patker = $("#Patker");
+const button1 = $("#pButton");
+const gameBroken = $("#GameBroken");
+const iQ = $("#IQ");
+const button2 = $("#WUT");
+const db = firebase.firestore();
 
-pButton.addEventListener("click", () => {
-  document.querySelector("#Pakter").classList.add("show");
+db.settings({
+  timestampsInSnapshots: true;
 })
 
-gifButton.addEventListener("click", () => {
+brawl1.hide();
+patker.hide();
+gameBroken.hide();
+iQ.hide();
+
+function getComments(){
+  db.collection("comments").get().then(snapshot => {
+    snapshot.docs.forEach((docs, index) => {
+      $("#commentWapper").append(docs);
+    })
+  })
+}
+
+button.click(function(){
+  // brawl1.animate({"width":"1800px", "height" :"1000px", "opacity": "1"})
+  brawl1.toggle();
+})
+
+button1.click(function(){
+  // patker.animate({"width":"600px", "height":"600px", "opacity":"1"})
+  patker.toggle();
+})
+
+button2.click(function(){
+  gameBroken.toggle();
+  iQ.toggle();
+  // gameBroken.animate({"width":"900", "height":"600px", "opacity":"1"})
+  // iQ.animate({"width":"900", "height":"600px", "opacity":"1"})
 })
