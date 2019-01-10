48d8cfac5a127e88d0aa43277713706e881abf7f
unknown <bafford1234@gmail.com>
2019-01-10 11:33:19 -0500

    firebase

--- javascript.js
+++ javascript.js
@@ -5,38 +5,38 @@ const button1 = jquery("#pButton");
 const gameBroken = jquery("#GameBroken");
 const iQ = jquery("#IQ");
 const button2 = jquery("#WUT");
-//const db = firebase.firestore();
+const db = firebase.firestore();
 
-//db.settings({
-//   timestampsInSnapshots: true
-// })
-//
-// brawl1.hide();
-// patker.hide();
-// gameBroken.hide();
-// iQ.hide();
+db.settings({
+  timestampsInSnapshots: true
+})
 
-// function getComments(){
-//   db.collection("comments").get().then(snapshot => {
-//     snapshot.docs.forEach((docs, index) => {
-//       jquery("#commentWapper").append(docs);
-//     })
-//   })
-// }
+brawl1.hide();
+patker.hide();
+gameBroken.hide();
+iQ.hide();
+
+function getComments(){
+  db.collection("comments").get().then(snapshot => {
+    snapshot.docs.forEach((docs, index) => {
+      jquery("#commentWapper").append(docs);
+    })
+  })
+}
 
 button.click(function(){
-  // brawl1.animate({"width":"1800px", "height" :"1000px", "opacity": "1"})
+  brawl1.animate({"width":"1800px", "height" :"1000px", "opacity": "1"})
   brawl1.toggle();
 })
 
 button1.click(function(){
-  // patker.animate({"width":"600px", "height":"600px", "opacity":"1"})
+  patker.animate({"width":"600px", "height":"600px", "opacity":"1"})
   patker.toggle();
 })
 
 button2.click(function(){
   gameBroken.toggle();
   iQ.toggle();
-  // gameBroken.animate({"width":"900", "height":"600px", "opacity":"1"})
-  // iQ.animate({"width":"900", "height":"600px", "opacity":"1"})
+  gameBroken.animate({"width":"900", "height":"600px", "opacity":"1"})
+  iQ.animate({"width":"900", "height":"600px", "opacity":"1"})
 })
--- static/javascript.js
+++ static/javascript.js
@@ -5,24 +5,44 @@ const button1 = jQuery("#pButton");
 const gameBroken = jQuery("#GameBroken");
 const iQ = jQuery("#IQ");
 const button2 = jQuery("#WUT");
-// const db = firebase.firestore();
-//
-// db.settings({
-//   timestampsInSnapshots: true
-// })
+const db = firebase.firestore();
+const commentIn = $("#commentIn");
+const commentSubmit = $("#commentSubmit");
+
+function submitComment(message){
+  db.collection("comments").add({
+    comment:message
+  })
+}
+
+commentSubmit.click(() => {
+  submitComment(commentIn.val());
+})
+
+db.settings({
+  timestampsInSnapshots: true
+})
 //
 // brawl1.hide();
 // patker.hide();
 // gameBroken.hide();
 // iQ.hide();
 //
-// function getComments(){
-//   db.collection("comments").get().then(snapshot => {
-//     snapshot.docs.forEach((docs, index) => {
-//       $("#commentWapper").append(docs);
-//     })
-//   })
-// }
+function genComment(message){
+  return `
+  <div class="comment"><h1>${message}</h1></div>
+  `
+}
+
+function getComments(){
+  db.collection("comments").get().then(snapshot => {
+    snapshot.docs.forEach((doc, index) => {
+      console.log(doc.data().comment);
+      $("#commentWrapper").append(genComment(doc.data().comment));
+    })
+  })
+}
+getComments();
 
 button.click(function(){
   // brawl1.animate({"width":"1800px", "height" :"1000px", "opacity": "1"})
